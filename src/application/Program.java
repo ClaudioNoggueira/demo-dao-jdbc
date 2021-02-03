d02895ae39c0f750446449d2ffee2ae0109a4af9
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-02-03 15:25:48 -0300

    SellerDAOJDBC e DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Program {
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Livros");
 		System.out.println(dep);
+
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
 		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, dep);
 		System.out.println(seller);
