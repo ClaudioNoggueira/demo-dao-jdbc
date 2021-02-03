843ee072401d3a80d85f4057cd7cb5e43aa7afcd
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-02-03 15:04:31 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Livros");
 		System.out.println(dep);
+
+		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, dep);
+		System.out.println(seller);
 	}
 }
