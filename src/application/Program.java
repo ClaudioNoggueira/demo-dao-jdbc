d077c2b9ee31e99d71244459e7d11baa126cca6d
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-02-03 14:50:09 -0300

    Classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Department dep = new Department(1, "Livros");
+		System.out.println(dep);
 	}
 }
